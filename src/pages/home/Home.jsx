a4da29e5428d712b717d58251b610033b3c06c4f
Reegan Reethas <reegan.reethas@diconium.com>
2021-09-24 23:14:57 +0200

    initial commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavSideBar, PrimarySearchBar, Header } from "../../components/Index";
+
+export function Home() {
+  return (
+    <div>
+      <NavSideBar />
+      <PrimarySearchBar />
+    </div>
+  );
+}
